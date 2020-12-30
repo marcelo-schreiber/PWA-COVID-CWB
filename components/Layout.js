98eded08611369f517f90ac0576d3e900c22bfa8
Marcelo Augusto Rissette Schreiber <maraugrissch@gmail.com>
2020-12-30 18:22:23 -0300

    Fix: correct metadata (for pwa)

--- components/Layout.js
+++ components/Layout.js
@@ -20,9 +20,9 @@ function Layout({ title, children }) {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1"
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-
+        <meta name="description" content="Veja como Curitiba lida com o corona vírus." />
+        <meta name="keywords" content="covid, corona, virus, cwb, curitiba, dados, estatística" />
+        <meta name="author" content="Marcelo Schreiber">
         <link rel="manifest" href="/manifest.json" />
         <link href="/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
         <link href="/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
