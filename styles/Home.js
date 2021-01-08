fdc1d7ca29acd4765c0348f5aedf87bcc6673f59
Marcelo Augusto Rissette Schreiber <maraugrissch@gmail.com>
2021-01-08 00:16:44 -0300

    Fix: h3 > h2 & theme color

--- styles/Home.js
+++ styles/Home.js
@@ -18,7 +18,7 @@ export const TitleLogo = styled.h1`
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
-export const Slogan = styled.h3`
+export const Slogan = styled.h2`
   font-weight: normal;
   font-size: 4.265rem;
   line-height: 5.5rem;
